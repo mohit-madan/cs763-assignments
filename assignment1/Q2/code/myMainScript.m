ea3da5c708b4385bc8ff806c6cf0849e018ae84a
mohit-madan <mohitmadan71@gmail.com>
2018-01-24 20:11:17 +0530

    added ques2.. incomplete

--- assignment1/Q2/code/myMainScript.m
+++ assignment1/Q2/code/myMainScript.m
@@ -3,4 +3,46 @@
 tic;
 %% Your code here
 
+input_img = imread('../input/rad_checkerbox.jpg');
+input_img = im2double(input_img); 
+
+[h,w,d] = size(input_img); %%height and width 
+
+h_2 = floor(h/2);
+w_2 = floor(w/2);
+
+r = zeros(h,w);
+
+for i = 1:size(r,1)
+    for j = 1:size(r,2)
+          r(i,j) = sqrt((((h+1)/2 - i)/h_2).^2 + (((w+1)/2 - j)/w_2).^2);
+    end
+end
+
+distort = (1 + r + 0.5*(r.*r)); %distortion matrix
+
+xd_cord = meshgrid(-w_2:w_2,-h_2:h_2)/w_2; %distorted - initial one
+yd_cord = meshgrid(-h_2:h_2,-w_2:w_2)'/h_2; %distorted
+
+x_cord = xd_cord; %initialisation
+y_cord = yd_cord;
+
+%%iterations for getting the undistorted matrix
+for i = 1:5
+
+	del_x = x_cord.*(distort) - x_cord;
+	del_y = y_cord.*(distort) - y_cord;
+
+	x_cord = xd_cord - del_x;
+	y_cord = yd_cord - del_y;
+
+end    
+
+%%plotting in rgb
+final_img(:,:,1) = interp2(xd_cord,yd_cord,input_img(:,:,1),x_cord,y_cord);
+final_img(:,:,2) = interp2(xd_cord,yd_cord,input_img(:,:,2),x_cord,y_cord);
+final_img(:,:,3) = interp2(xd_cord,yd_cord,input_img(:,:,3),x_cord,y_cord);
+ 
+output = image(final_img);
+
 toc;
