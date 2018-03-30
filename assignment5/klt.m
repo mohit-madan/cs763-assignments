cf4540cbf3a70577f8207c99b764b84945eff12b
Sachin Goyal <sach.goyalsachin@gmail.com>
2018-03-30 22:34:56 +0530

    b1 b1 magnitude too high

--- assignment5/klt.m
+++ assignment5/klt.m
@@ -1,7 +1,7 @@
-n_iter = 20;
+n_iter = 1;
 Template_frame = imread('/home/krishna/Desktop/Sachin/cs763/HW5/input/1.jpg');
 Template_frame = padarray(Template_frame,[3,3],'both');
-Present_frame = imread('/home/krishna/Desktop/Sachin/cs763/HW5/input/2.jpg');
+Present_frame = imread('/home/krishna/Desktop/Sachin/cs763/HW5/input/4.jpg');
 Present_frame = padarray(Present_frame,[3,3],'both');
 [p1,p2] = size(Present_frame);
 
@@ -14,8 +14,8 @@ all_eigen = [];
 for i = 1:1%size(Hpnts,1)
     %ith feature point
     H = 0;
-    for j = 0%-3:3
-        for k = 0%-3:3
+    for j = -3:3
+        for k = -3:3
             x = Hpnts(i,1) + j;
             y = Hpnts(i,2) + k ;
             Ix = Gtx(y,x);
@@ -24,9 +24,6 @@ for i = 1:1%size(Hpnts,1)
             H = H + h*h';
         end
     end
-    i
-    H = H;
-    inv(H);
     e = eig(H);
     all_eigen = [all_eigen; e'];
 end
@@ -41,18 +38,17 @@ final_pnts = [584,279];%say
 p = [1 0 0 0 1 0];%a1 a2 b1 a3 a4 b2
 for i = 1:1
     for j = 1:n_iter
-        j
         tform = affine2d([p(1) p(4) 0;p(2) p(5) 0; p(3) p(6) 1]);
         Present_frame_w = imwarp(Present_frame,tform);
         Present_frame_w = imresize(Present_frame_w,[p1,p2]);
 
         TP = [final_pnts(i,1) , final_pnts(i,2) , 1]';
 
-        T = Template_frame(TP(2,1)-3:TP(2,1)+3 , TP(1,1)-3:TP(1,1)+3);  
+        T = Template_frame(TP(2,1)-3:TP(2,1)+3 , TP(1,1)-3:TP(1,1)+3) 
 %         subplot(2,2,1)
 %         imshow(T)
 
-        I_w = Present_frame_w(TP(2,1)-3:TP(2,1)+3 , TP(1,1)-3:TP(1,1)+3);  
+        I_w = Present_frame_w(TP(2,1)-3:TP(2,1)+3 , TP(1,1)-3:TP(1,1)+3)  
 %         subplot(2,2,2)
 %         imshow(I_w)
         [m,n] = size(I_w);
@@ -73,20 +69,17 @@ for i = 1:1
         g = 1;
         for j=1:n
             for k=1:m
-                Sum = Sum + double(diff(k,j)) * second_factor(g,:) %double for data type
+                Sum = Sum + double(diff(k,j)) * second_factor(g,:); %double for data type
                 g=g+1;
             end
         end
-           
         
-
-
         H = 0;
         i=1;
         for j = -3:3
             for k = -3:3
-                x = final_pnts(i,1) ;
-                y = final_pnts(i,2);
+                x = final_pnts(i,1) + j;
+                y = final_pnts(i,2) + k;
                 Ix = Gtx(y,x);
                 Iy = Gty(y,x); 
                 h = [x*Ix y*Ix Ix x*Iy y*Iy Iy]';
@@ -94,8 +87,8 @@ for i = 1:1
             end
         end
         
-        Sum
-        del_p = Sum * inv(H);%you do this sachin
+        Sum;
+        del_p = Sum * inv(H)%you do this sachin
         p = p+del_p
     end
 end
